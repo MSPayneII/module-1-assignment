b7d65c75de5a66b1716a12caf3926c4b028773e5
Michael Payne <mpaynei@umich.edu>
2021-10-15 16:26:06 -0400

    added commments

--- password.js
+++ password.js
@@ -14,16 +14,18 @@ reader.question(
   function (input) {
     userInput = input;
 
-    // if the user's input is blank
+    // If the user's input is blank/zero characters in length
     if (userInput.length === 0) {
       console.log(
         "You must enter a password. Please restart the program and enter a password."
       );
     } else if (userInput.length >= 10) {
+      // If user input is 10 or more characters long
       console.log(
         " Your password is validated!. It meets the necessary amount of characters needed."
       );
     } else if (userInput.length > 0 && userInput.length < 10) {
+      // If user input is 1-9 characters long
       console.log(
         "Passwords must have a mininum of 10 characters to be validated."
       );
